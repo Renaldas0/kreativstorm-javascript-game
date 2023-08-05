360b89eebd0f5edfa16efe06b5bba7b3da02de55
MiguelCarmonaF <edgarmiguelcarmonafelix@gmail.com>
2023-08-05 11:37:59 -0600

    Fixing: Null handling, now shows what you entered i(if its wrong) and message after round 1 changed

--- script/script.js
+++ script/script.js
@@ -5,7 +5,9 @@ let computerSelection = ['rock', 'paper', 'scissors'],
     playerSelection = "",
     computerScore = 0,
     playerScore = 0,
-    draws = 0;
+    draws = 0,
+    roundNumber = 0;
+
 
 function computerPlay() {
     return randomChoice = computerSelection[Math.floor(Math.random() * computerSelection.length)]; //0 = Rock, 1 = paper, 2 = scissors
@@ -33,7 +35,8 @@ function round(randomChoice, playerSelection) {
 
 function playerPlay(round){
     do {
-        playerSelection =
+        if (round==0){
+            playerSelection =
             window.prompt("Welcome to Rock, Paper, Scissors." +
                 "\nStrategy will take you far here, choose wisely." +
                 "\nbetween: Rock, Paper or Scissors" +
@@ -41,23 +44,38 @@ function playerPlay(round){
                 "\nLet's start" + "\nRound:" + (round + 1) +
                 "\n\nNote: If the round doesn't change it means you entered a wrong value, make sure you enter one of the 3 mentioned" +
                 "\nPress cancel to exit the game");
+        } else {
+            playerSelection =
+            window.prompt("Choose again!" + "\nRound:" + (round + 1) +
+                "\n\nNote: If the round doesn't change it means you entered a wrong value, make sure you enter one of the 3 mentioned" +
+                "\nPress cancel to exit the game");
+        }
+        if (playerSelection===null){
+            return roundNumber=4;   
+        }
         playerSelection = playerSelection.toLowerCase().trim();
+        if(!(playerSelection == "rock" || playerSelection == "paper" || playerSelection == "scissors")){
+            window.alert("You write: " + playerSelection + "\n Please make sure of write only paper, rock or scissor \n Round does not change");
+        }
     } while (!(playerSelection == "rock" || playerSelection == "paper" || playerSelection == "scissors"))
 }
 
 function EndGame(round){
-    if (round == 4) {
+    if (round == 4 && playerSelection != null) {
         return computerScore > playerScore ? window.alert(`Computer score: ${computerScore} \n Player score: ${playerScore} \n Draws: ${draws} \n You lose the game 😞 \n reload the page to play again!`)
             : computerScore == playerScore ? window.alert(`Computer score: ${computerScore} \n Player score: ${playerScore} \n Draws: ${draws} \n It seems that the game ended in a tie. Reload the page to play again!`)
                 : window.alert(`Computer score: ${computerScore} \n Player score: ${playerScore} \n Draws: "${draws} \n You win the game 🥳 pro!. Reload the page to play again!`);
-    };
+    } else if (round == 4 && playerSelection == null){
+        return window.alert("You finished the game, reload to play again"); 
+    }
 }
+
 function play() {
-    for (let i = 0; i < 5; i++) {
+    for (roundNumber = 0; roundNumber < 5; roundNumber++) {
         randomChoice = computerPlay();
-        playerPlay(i);
+        playerPlay(roundNumber);
         round(randomChoice, playerSelection);
-        EndGame(i);
+        EndGame(roundNumber);
     };
 };
 
